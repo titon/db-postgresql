791be67c44b85d2ebfe0b1527d74fbdb9cbf2e7c
Miles Johnson <mileswjohnson@gmail.com>
2014-01-15 22:01:38 -0800

    Fix env password

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,7 +25,7 @@ $db = [
     'database' => 'titon_test',
     'host' => '127.0.0.1',
     'user' => 'postgres',
-    'pass' => ''
+    'pass' => getenv('TRAVIS') ? '' : 'test123'
 ];
 
 Titon\Common\Config::set('db', $db);
